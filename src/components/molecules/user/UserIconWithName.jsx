91d37d6a50a645b9a91adc1bf5a23c537926eb7a
RIKI <ab.jpnet.01@gmail.com>
2022-01-10 18:15:13 +0900

    recoil complate

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,10 +1,13 @@
 import { memo, useContext } from "react";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { UserContext } from "../../../providers/UserProvider";
+import { userState } from "../../../store/userState";
 
 export const UserIconWithName = memo((props) => {
   const { image, name } = props;
-  const { userInfo } = useContext(UserContext);
+  // const { userInfo } = useContext(UserContext);
+  const userInfo = useRecoilValue(userState);
   const isAdmin = userInfo ? userInfo.isAdmin : false;
 
   return (
