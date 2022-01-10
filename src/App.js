dfc703d1585312d8c8aa6e977331dea8a89af7b1
RIKI <ab.jpnet.01@gmail.com>
2022-01-10 16:51:14 +0900

    sample completion

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
-  const user = {
-    name: "じゃけぇ",
-    image: "https://source.unsplash.com/yihlaRCCvd4",
-    email: "12345@example.com",
-    phone: "090-1111-2222",
-    company: {
-      name: "テスト株式会社"
-    },
-    website: "https://google.com"
-  };
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
